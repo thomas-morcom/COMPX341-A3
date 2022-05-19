16f2bb0e24484d05b1d87aa1812745e8c3339f41
Thomas Morcom <thomascmorcom@gmail.com>
2022-05-19 16:58:43 +1200

    “COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,5 +1,21 @@
-npm install
 
+
+npm install
+if [ $? -ne 0 ] 
+then
+    echo Install Failed
+    exit
+fi
 npm run build
+if [ $? -ne 0 ] 
+then
+    echo Build Failed
+    exit
+fi
+
+git pull
+git add .
+git commit -m "“COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+git push
 
 npm run start
