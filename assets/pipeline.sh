228f2de10237dfd3a5cafdfeff5885e2a0940405
Thomas Morcom <thomascmorcom@gmail.com>
2022-05-19 17:44:00 +1200

    COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -11,6 +11,11 @@ then
     exit
 fi
 
+if [ $# -eq 0 ]
+then 
+    echo No commit message was given
+    exit
+fi
 git pull
 git add .
 git commit -m "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
